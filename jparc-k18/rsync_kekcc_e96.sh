9c44dd52cf6901719c2478f758a98bf650a5b601
Shuhei Hayakawa <shuhei.hayakawa.c8@tohoku.ac.jp>
2025-02-12 18:38:36 +0900

    Add jparc-k18.

---
+++ jparc-k18/rsync_kekcc_e96.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+#set -e
+SCRIPT_NAME=$(basename "$0")
+
+if pidof -o $$ -x "$SCRIPT_NAME" > /dev/null; then
+    echo "$SCRIPT_NAME is already running."
+    exit 1
+fi
+
+
+src=(
+    /mnt/raid/e96_bench
+    /mnt/raid/e96_2025jan
+    # /mnt/raid/software/param.git
+    # /mnt/raid/subdata
+)
+
+host=login.cc.kek.jp
+dest_disk=/group/had/sks/E96/JPARC2025Jan
+dest_tape=/hsm/had/sks/E96/JPARC2025Jan
+
+# dest_disk=$HOME/group/E70/JPARC2024May
+# dest_tape=$HOME/hsm/E70/JPARC2024May
+
+if [ ! -f $HOME/.pswd ]; then exit; fi
+sshpass="sshpass -f $HOME/.pswd ssh -o Compression=no -x -l hayashu"
+# sshpass="sshpass -p $pass ssh -o PubKeyAuthentication=no -l $user"
+
+#rsync="rsync -rlptDvhP --update --partial --bwlimit=20M --exclude=*.dat"
+#rsync="rsync -rlptDvhP --delete"
+rsync="rsync -ahz --partial --whole-file"
+#rsync="rsync -rlptDvhP"
+
+while true
+do
+    for s in ${src[@]}
+    do
+	if [ ! -e $s ]; then continue; fi
+	if ( echo $s | grep -sq e96 && ls $s | grep -sq lock ); then
+	    echo "$s -> skip"
+	    echo
+	    continue
+	fi
+	if echo $s | grep -sq e96; then
+	    dest=$host:$dest_tape
+	    echo "$s -> $dest"
+	    $rsync --exclude="$s/run*" -e "$sshpass" $s $dest
+	    tmp_include=/tmp/include-e96
+	    find $s/run* -perm 444 > $tmp_include
+	    $rsync --include-from=$tmp_include --exclude="*" -e "$sshpass" $s $dest
+	else
+	    dest=$host:$dest_disk
+	    echo "$s -> $dest"
+	    $rsync -e "$sshpass" $s $dest
+	fi
+	#ionice -c 2 -n 7 nice -n 19
+    done
+    date
+
+    break
+    
+    echo waiting...
+    sleep 300
+done
